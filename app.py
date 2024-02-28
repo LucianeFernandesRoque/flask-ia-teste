a751827a945d77820cae6a1bcb8d6405c7c9f08a
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-02-28 19:52:20 -0300

    Fix App include

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+
+app = Flask(__name__)
 import requests, os, uuid, json
 from dotenv import load_dotenv
 load_dotenv()
